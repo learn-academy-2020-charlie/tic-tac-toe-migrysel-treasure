c719aecd51152ff9b2515a4fcebc8dd99aa0b3d3
rmdpalo <rmd.palo@gmail.com>
2020-07-15 10:33:59 -0700

    user stories 1-3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react'
 import Square from './components/Square'
 import './App.css'
-
+//TIC TAC TOE
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: ["", "", "", "", "", "", "", "", ""],
+      //counter is to keep track of whose turn it is
+      counter: 0
     }
   }
 
+  //if counter === 6, alert game over. ___ wins, cat game(no one wins)
+  
+  //As a user, when someone has won the game (3 squares in a row: horizontally, vertically, or diagonally) I should see a notice telling me who won.
+
+handleClick = (index, value) => {
+  let { squares, counter } = this.state
+  //user should be able to click on a square to mark it
+  // counter.length < 5
+  this.setState({squares: squares, counter: counter })
+  console.log(counter);
+  if(squares[index] === ""){
+  //use a conditional using modulo to set odd and even player.
+  if (counter % 2 === 0) { 
+    squares[index] = "X"
+    counter = counter + 1
+    this.setState({squares: squares, counter: counter})
+  } else if (counter % 2 !== 0){
+    squares[index] = "O"
+    counter = counter + 1
+    this.setState({squares: squares, counter: counter})
+  } 
+  }
+}
+
   render(){
-    return(
+    let box = this.state.squares.map((value, index)=> {
+      return (
+        <Square 
+          value = { value }
+          index = { index }
+          handleClick = { this.handleClick }
+          squares = { this.squares }
+          key = { index }
+        />
+      )
+    })
+    return (
       <React.Fragment>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div id="gameboard">
+          { box }
+        </div>
       </React.Fragment>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-
+//THIS IS FOR TIC TAC TOE
 class Square extends Component{
+
+    cursorClick = () => {
+      this.props.handleClick(this.props.index, this.props.value)
+    }
+
   render(){
     return(
       <React.Fragment>
-        <div id="square"></div>
+        <div id="square" onClick = {this.cursorClick}>
+          {this.props.value}
+        </div>
       </React.Fragment>
     )
   }
