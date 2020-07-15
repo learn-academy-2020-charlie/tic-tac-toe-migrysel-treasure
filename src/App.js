106d31bf3c02deb6b2b50c42a25193c5fc1dc373
selamhabteab <selamhabteab@gmail.com>
2020-07-15 11:58:14 -0700

    still stuck on user task3

--- src/App.js
+++ src/App.js
@@ -7,14 +7,31 @@ class App extends Component{
     super(props)
     this.state = {
       squares: ["", "", "", "", "", "", "", "", ""],
-      //counter is to keep track of whose turn it is
-      counter: 0
+      counter: 0,
+      winningIndex: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8], 
+        [0,3,6],
+        [1,4,7], 
+        [2,5,8],
+        [0,4,8], 
+        [2,4,6],
+      ]
     }
   }
-
-  //if counter === 6, alert game over. ___ wins, cat game(no one wins)
   
+  //if counter === 6, alert game over. ___ wins, cat game(no one wins)
+
   //As a user, when someone has won the game (3 squares in a row: horizontally, vertically, or diagonally) I should see a notice telling me who won.
+  // winIndex: [0,1,2] & [3,4,5]  &[6,7,8]
+  // vertical: [0,3,6] & [1,4,7] & [2,5,8]
+  // diagonal: [0,4,8] & [2,4,6]
+  //use a map function for winningIndex to give access to every array inside and use filter method 
+  //squares[index].filter()
+  //map through larger array in order to create each value of that map function to access smaller array. then destructure each smaller array to assign a variable to hold each value. 
+  //[a,b,c] does a=x b=x c=x
+  //add item in state for currentplayer in order to toggle between each player 
 
 handleClick = (index, value) => {
   let { squares, counter } = this.state
@@ -24,7 +41,7 @@ handleClick = (index, value) => {
   console.log(counter);
   if(squares[index] === ""){
   //use a conditional using modulo to set odd and even player.
-  if (counter % 2 === 0) { 
+  if (counter % 2 === 0) {
     squares[index] = "X"
     counter = counter + 1
     this.setState({squares: squares, counter: counter})
@@ -32,14 +49,23 @@ handleClick = (index, value) => {
     squares[index] = "O"
     counter = counter + 1
     this.setState({squares: squares, counter: counter})
-  } 
   }
+  }
+  // if (squares[index] = "X" && squares[index] = this.state.winningIndex) {
+  //   alert ("'X' Wins!")
+  // } else if (
+  //   ) {
+  //   alert ("'O' Wins!")
+  //}
+  // if (squares[0]=== squares[1] === squares[2]) {
+  //   alert "You Win!"
+  // }
 }
 
   render(){
     let box = this.state.squares.map((value, index)=> {
       return (
-        <Square 
+        <Square
           value = { value }
           index = { index }
           handleClick = { this.handleClick }
